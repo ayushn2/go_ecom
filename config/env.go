4ed231a7c29e2f18f9d80532342abf7d662bce09
ayushn2 <iayush.n2@gmail.com>
2024-10-06 17:27:10 +0530

    Fixed an issue with making connection with database

--- config/env.go
+++ config/env.go
@@ -21,7 +21,7 @@ func initConfig() Config{
 		PublicHost: getEnv("PUBLIC_HOST","http://localhost"),
 		Port : getEnv("POST","8080"),
 		DBUser : getEnv("DB_USER","root"),
-		DBPassword: getEnv("DB_PASSWORD","mypassword"),
+		DBPassword: getEnv("DB_PASSWORD","#Ayush2004"),
 		DBAddress: fmt.Sprintf("%s:%s",getEnv("DB_HOST","127.0.0.1"),getEnv("DB_PORT","3306")),
 		DBName: getEnv("DB_NAME","go_ecom"),
 	}
